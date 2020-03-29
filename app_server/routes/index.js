09fc3c8a8124a9b111c4e245c79d45444c52a311
Jonathan Sullivan <jonathan.k.sullivan@gmail.com>
2020-03-29 00:06:18 -0700

    Add the view templates

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-const ctrlMain = require('../controllers/main');
+const ctrlLocation = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', ctrlLocation.homelist);
+router.get('/location', ctrlLocation.locationInfo);
+router.get('/location/review/new', ctrlLocation.addReview);
+
+/*Other pages*/
+router.get('/about', ctrlOthers.about)
 
 module.exports = router;

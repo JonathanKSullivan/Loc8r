946b6ccb4722c0b80e5b6a6a21ec76f13c47e2d1
Jonathan Sullivan <jonathan.k.sullivan@gmail.com>
2020-03-30 05:51:36 -0700

    Use db data to render homepage 2

--- app_api/controllers/locations_util.js
+++ app_api/controllers/locations_util.js
@@ -4,17 +4,14 @@ const hoursToMinutes = hours => {
 }
 
 const convertTimeStringToMinutesFromMidnight = (time, isDisabled) => {
-  console.log("DEBUG-isDisabled-"+isDisabled)
   if(!isDisabled){
     const timeNum = time.split(' ')[0]
     const hour = parseInt(timeNum.split(':')[0])
     const minutes = parseInt(timeNum.split(':')[1])
     const amPm = time.split(' ')[1]
-    // console.log("DEBUG-"+amPm)
     let minutesFromMidnight = amPm.toLowerCase() === 'pm' ? hoursToMinutes(12) : 0
     minutesFromMidnight += hoursToMinutes(hour)
     minutesFromMidnight += minutes
-    console.log("DEBUG-minutesFromMidnight-"+minutesFromMidnight)
     return minutesFromMidnight
   }
 }
@@ -34,10 +31,7 @@ const zipOpeningTimes = (days, openings, closings, closeds) => {
 const setOpeningTimes = (days, openings, closings, closeds) => {
   const openingsTimes = zipOpeningTimes(days, openings, closings, closeds)
   return openingsTimes.map( openingsTime => {
-    console.log("DEBUG-"+openingsTime[1]+" "+!openingsTime[3].includes("true"))
-    console.log("DEBUG-"+openingsTime[2]+" "+!openingsTime[3].includes("true"))
     const closed_val = openingsTime[3].includes("true")
-    console.log("DEBUG-close"+convertTimeStringToMinutesFromMidnight(openingsTime[1], closed_val))
     return {
       days: openingsTime[0],
       opening: convertTimeStringToMinutesFromMidnight(openingsTime[1], closed_val),
